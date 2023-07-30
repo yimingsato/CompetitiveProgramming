bcf3e8377d2d7df55865f5123aca306d02a13d87
Lucas Chen <109487333+yimingsato@users.noreply.github.com>
2023-07-30 11:29:26 -0400

    Create '22 J1.java

---
+++ CCC/'22 J1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class CupcakeParty {
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(x*8+y*3-28);
+    }
+}
