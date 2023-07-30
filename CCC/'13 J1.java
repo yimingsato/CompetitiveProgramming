1e6b85a469c4c6c6b7caf39f561ba6b78dba9be5
Lucas Chen <109487333+yimingsato@users.noreply.github.com>
2023-07-30 11:31:32 -0400

    Create '13 J1.java

---
+++ CCC/'13 J1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class NextInLine {
+    public static void main(String[] Args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = y-x;
+        System.out.println(y+z);
+
+    }
+}
