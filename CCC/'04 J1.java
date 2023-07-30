46f219f606d28f029fefd0d24a592bb9b70fed18
Lucas Chen <109487333+yimingsato@users.noreply.github.com>
2023-07-30 11:30:20 -0400

    Create '04 J1.java

---
+++ CCC/'04 J1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+public class Squares {
+    public static void main(String[] Args){
+    Scanner sc = new Scanner(System.in);
+    int x = sc.nextInt();
+    System.out.println("The largest square has side length " + (int)Math.sqrt(x) + ".");
+    }
+}
